e4d25eab10c29e5ed06fac8c7df2cff2f5fd2d21
Deni Cristian <deni@denicristian.com.br>
2022-10-09 14:54:24 -0300

    Summary
    Novos arquivos

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 function App() {
+  const teste = 1123
   return (
     <>
-      <div>
-        teste
+      <div className="color[#FFFFFF]">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas excepturi est fugit commodi ipsum fugiat quis
+        exercitationem dolor blanditiis? Ipsum earum eligendi deserunt inventore aperiam dolorum perspiciatis
+        reprehenderit nihil animi nemo. Earum corporis harum quibusdam tempore reiciendis debitis quae sit, assumenda
+        aperiam quas voluptatem ratione culpa beatae quisquam vel, vero animi eius delectus autem ad. Dolorum explicabo
+        hic, unde, reprehenderit architecto cumque perferendis beatae alias molestias rem, repudiandae suscipit magnam
+        autem eveniet dolor fugiat nostrum adipisci tenetur officia corporis error. Fuga blanditiis inventore labore
+        sint autem numquam tempore voluptatum optio ut nobis necessitatibus ipsam, porro id sequi voluptatem quod
+        expedita, amet rem esse delectus quo ipsum nemo. Maxime eius facilis voluptate minima quis odio officiis
+        excepturi omnis officia exercitationem qui cumque blanditiis laudantium cum iste dolorum dolore, delectus eaque.
+        Reiciendis earum officia veniam eaque voluptatum sed, harum a cum veritatis, dolorem, enim aliquid libero neque
+        incidunt nam! Sed ullam repellat tempora suscipit quaerat porro doloribus nemo repudiandae eius velit! Repellat
+        reprehenderit labore quod voluptates exercitationem odio atque explicabo adipisci consectetur laborum rerum
+        excepturi praesentium ipsum repudiandae molestiae natus similique nisi dicta hic aliquid et reiciendis beatae,
+        delectus amet! Nihil quisquam totam odio, explicabo perspiciatis ex maiores culpa excepturi perferendis itaque?
       </div>
+      <button></button>
+
+      <div></div>
     </>
   )
 }
